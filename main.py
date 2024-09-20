39019e12ce72a8c0209446f7df83885081753cc0
kido2k3 <101075468+kido2k3@users.noreply.github.com>
2024-09-20 23:49:34 +0700

    receiver updated v1

--- main.py
+++ main.py
@@ -1,121 +1,77 @@
-# sender
-"""
-0: idle
-1: send data
-2: wait 2s for ack value. If no ack, resend data
-resend over 2 times -> turn led(0, 0) on in 1s
-received ack -> turn led(1, 1) on in 1s
-"""
+# receiver
+radio.set_group(5)
 
+def toggle_led():
+    for i in range(3):
+        led.toggle(0, i)
+    led.toggle(0, 4)
 
+heat_timer = 0
+heat_st = 0
+def heat_warning_fsm(cur_st: int):
+    global heat_timer
+    if cur_st == 0:
+        for i in range(3):
+            led.unplot(0, i)
+        led.unplot(0, 4)
+        heat_timer = 0
+    elif cur_st == 1:
+        heat_timer = heat_timer + 1
+        if heat_timer == 25:
+            toggle_led()
+            heat_timer = 0
+
+def light_off():
+    for i in range(2, 5):
+        for j in range(2, 5):
+            led.unplot(i, j)
+def light_on():
+    for i in range(2, 5):
+        for j in range(2, 5):
+            led.plot(i, j)
+light_st = 0
+def light_warning_fsm(cur_st: int):
+    if cur_st == 0:
+        light_off()
+    elif cur_st == 1:
+        light_on()
 def on_received_number(receivedNumber):
-    global ack
-    ack = receivedNumber
-    # for testing
+    global heat_st, light_st
+    if receivedNumber == 1:
+        heat_st = 1
+    elif receivedNumber == 2:
+        heat_st = 0
+    elif receivedNumber == 4:
+        light_st = 0
+    elif receivedNumber == 3:
+        light_st = 1
     if receivedNumber in [1, 2, 3, 4]:
         radio.send_number(5)
 radio.on_received_number(on_received_number)
 
-def sample_data():
-    global tem_value, light_value
-    tem_value = tem_value + input.temperature()
-    light_value = light_value + input.light_level()
-def send_data(data: number):
-    radio.send_number(data)
-def fsm_led(led_st: number, led_timer: number, x: number, y: number):
-    if led_st == 1:
-        led.plot(x, y)
-        led_timer += 0 - 1
-        if led_timer <= 0:
-            led_st = 0
-            led_timer = 100
-    elif led_st == 0:
-        led.unplot(x, y)
-    return led_st, led_timer
-def fsm_sender():
-    global sender_st, send_time, received, ack, led11_st, timer_sender, led00_st
-    if sender_st == 0:
-        if received:
-            sender_st = 1
-            send_time = 0
-    elif sender_st == 1:
-        send_time += 1
-        send_data(received)
-        if ack == 5:
-            received = 0
-            sender_st = 0
-            ack = 0
-            led11_st = 1
-        else:
-            timer_sender = 200
-            sender_st = 2
-    elif sender_st == 2:
-        timer_sender += 0 - 1
-        if ack == 5:
-            received = 0
-            sender_st = 0
-            ack = 0
-            led11_st = 1
-        elif timer_sender <= 0:
-            sender_st = 1
-        elif send_time >= 3:
-            received = 0
-            sender_st = 0
-            led00_st = 1
-cnt = 0
-cur_st = 0
-led00_st = 0
-led11_st = 0
-send_time = 0
-received = 0
-sender_st = 0
-led_st2 = 0
-led_timer2 = 0
-light_value = 0
-tem_value = 0
-ack = 0
-timer_sender = 0
-TEM_THRESHOLD = 40
-LIGHT_THRESHOLD = 200
-radio.set_group(5)
-timer_sender = 200
-led00_timer = 100
-led11_timer = 100
-
-# 0: sample data
-# 1: handle data and send signal
 def on_forever():
-    global cnt, cur_st, received, tem_value, light_value
-    global led00_st, led00_timer, led11_st, led11_timer
-    if cur_st == 0:
-        sample_data()
-        cnt = cnt + 1
-        if cnt == 500:
-            cnt = 0
-            cur_st = 1
-    elif cur_st == 1:
-        tem_avg = tem_value / 500
-        light_avg = light_value / 500
-        if tem_avg > TEM_THRESHOLD:
-            # heat warning
-            received = 1
-            print(1)
-        else:
-            received = 2
-            print(2)
-        if light_avg < LIGHT_THRESHOLD:
-            # light warning
-            received = 3
-            print(3)
-        else:
-            received = 4
-            print(4)
-        cur_st = 0
-        tem_value = 0
-        light_value = 0
-    fsm_sender()
-    led00_st, led00_timer = fsm_led(led00_st, led00_timer, 0, 0)
-    led11_st, led11_timer = fsm_led(led11_st, led11_timer, 1, 1)
-
+    global heat_st, light_st
+    heat_warning_fsm(heat_st)
+    light_warning_fsm(light_st)
     basic.pause(10)
 basic.forever(on_forever)
+
+# for demo
+def on_button_pressed_a():
+    # heat warning
+    radio.send_number(1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    # heat normal
+    radio.send_number(2)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+def on_button_pressed_ab():
+    # light warning
+    radio.send_number(3)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_logo_event_pressed():
+    # light normal
+    radio.send_number(4)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
--- main.ts
+++ main.ts
@@ -1,153 +1,96 @@
-//  sender
-/** 
-0: idle
-1: send data
-2: wait 2s for ack value. If no ack, resend data
-resend over 2 times -> turn led(0, 0) on in 1s
-received ack -> turn led(1, 1) on in 1s
+//  receiver
+radio.setGroup(5)
+function toggle_led() {
+    for (let i = 0; i < 3; i++) {
+        led.toggle(0, i)
+    }
+    led.toggle(0, 4)
+}
 
- */
+let heat_timer = 0
+let heat_st = 0
+function heat_warning_fsm(cur_st: number) {
+    
+    if (cur_st == 0) {
+        for (let i = 0; i < 3; i++) {
+            led.unplot(0, i)
+        }
+        led.unplot(0, 4)
+        heat_timer = 0
+    } else if (cur_st == 1) {
+        heat_timer = heat_timer + 1
+        if (heat_timer == 25) {
+            toggle_led()
+            heat_timer = 0
+        }
+        
+    }
+    
+}
+
+function light_off() {
+    for (let i = 2; i < 5; i++) {
+        for (let j = 2; j < 5; j++) {
+            led.unplot(i, j)
+        }
+    }
+}
+
+function light_on() {
+    for (let i = 2; i < 5; i++) {
+        for (let j = 2; j < 5; j++) {
+            led.plot(i, j)
+        }
+    }
+}
+
+let light_st = 0
+function light_warning_fsm(cur_st: number) {
+    if (cur_st == 0) {
+        light_off()
+    } else if (cur_st == 1) {
+        light_on()
+    }
+    
+}
+
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     
-    ack = receivedNumber
-    //  for testing
+    if (receivedNumber == 1) {
+        heat_st = 1
+    } else if (receivedNumber == 2) {
+        heat_st = 0
+    } else if (receivedNumber == 4) {
+        light_st = 0
+    } else if (receivedNumber == 3) {
+        light_st = 1
+    }
+    
     if ([1, 2, 3, 4].indexOf(receivedNumber) >= 0) {
         radio.sendNumber(5)
     }
     
 })
-function sample_data() {
-    
-    tem_value = tem_value + input.temperature()
-    light_value = light_value + input.lightLevel()
-}
-
-function send_data(data: number) {
-    radio.sendNumber(data)
-}
-
-function fsm_led(led_st: number, led_timer: number, x: number, y: number): number[] {
-    if (led_st == 1) {
-        led.plot(x, y)
-        led_timer += 0 - 1
-        if (led_timer <= 0) {
-            led_st = 0
-            led_timer = 100
-        }
-        
-    } else if (led_st == 0) {
-        led.unplot(x, y)
-    }
-    
-    return [led_st, led_timer]
-}
-
-function fsm_sender() {
-    
-    if (sender_st == 0) {
-        if (received) {
-            sender_st = 1
-            send_time = 0
-        }
-        
-    } else if (sender_st == 1) {
-        send_time += 1
-        send_data(received)
-        if (ack == 5) {
-            received = 0
-            sender_st = 0
-            ack = 0
-            led11_st = 1
-        } else {
-            timer_sender = 200
-            sender_st = 2
-        }
-        
-    } else if (sender_st == 2) {
-        timer_sender += 0 - 1
-        if (ack == 5) {
-            received = 0
-            sender_st = 0
-            ack = 0
-            led11_st = 1
-        } else if (timer_sender <= 0) {
-            sender_st = 1
-        } else if (send_time >= 3) {
-            received = 0
-            sender_st = 0
-            led00_st = 1
-        }
-        
-    }
-    
-}
-
-let cnt = 0
-let cur_st = 0
-let led00_st = 0
-let led11_st = 0
-let send_time = 0
-let received = 0
-let sender_st = 0
-let led_st2 = 0
-let led_timer2 = 0
-let light_value = 0
-let tem_value = 0
-let ack = 0
-let timer_sender = 0
-let TEM_THRESHOLD = 40
-let LIGHT_THRESHOLD = 200
-radio.setGroup(5)
-timer_sender = 200
-let led00_timer = 100
-let led11_timer = 100
-//  0: sample data
-//  1: handle data and send signal
 basic.forever(function on_forever() {
-    let tem_avg: number;
-    let light_avg: number;
     
-    
-    if (cur_st == 0) {
-        sample_data()
-        cnt = cnt + 1
-        if (cnt == 500) {
-            cnt = 0
-            cur_st = 1
-        }
-        
-    } else if (cur_st == 1) {
-        tem_avg = tem_value / 500
-        light_avg = light_value / 500
-        if (tem_avg > TEM_THRESHOLD) {
-            //  heat warning
-            received = 1
-            console.log(1)
-        } else {
-            received = 2
-            console.log(2)
-        }
-        
-        if (light_avg < LIGHT_THRESHOLD) {
-            //  light warning
-            received = 3
-            console.log(3)
-        } else {
-            received = 4
-            console.log(4)
-        }
-        
-        cur_st = 0
-        tem_value = 0
-        light_value = 0
-    }
-    
-    fsm_sender()
-    let ___tempvar4 = fsm_led(led00_st, led00_timer, 0, 0)
-    led00_st = ___tempvar4[0]
-    led00_timer = ___tempvar4[1]
-    let ___tempvar5 = fsm_led(led11_st, led11_timer, 1, 1)
-    led11_st = ___tempvar5[0]
-    led11_timer = ___tempvar5[1]
+    heat_warning_fsm(heat_st)
+    light_warning_fsm(light_st)
     basic.pause(10)
+})
+//  for demo
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    //  heat warning
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    //  heat normal
+    radio.sendNumber(2)
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    //  light warning
+    radio.sendNumber(3)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    //  light normal
+    radio.sendNumber(4)
 })
