8ee79817b77f8e950872a85e11b78a14009a2b13
kido2k3 <101075468+kido2k3@users.noreply.github.com>
2024-09-20 23:17:28 +0700

    sender updated v1

--- main.py
+++ main.py
@@ -1,49 +1,103 @@
-# receiver
+# sender
 radio.set_group(5)
 
-flag = 0
-buf = 1
+received = 0
+def on_gesture_logo_up():
+    global received
+    received = 1
+input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
+
+def on_gesture_tilt_left():
+    global received
+    received = 2
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
+def on_gesture_tilt_right():
+    global received
+    received = 3
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
+
+def on_gesture_logo_down():
+    global received
+    received = 4
+input.on_gesture(Gesture.LOGO_DOWN, on_gesture_logo_down)
+
+
+
+ack = 0
 def on_received_number(receivedNumber):
-    global flag, buf
-    flag = 1
-    buf = receivedNumber
-    if buf in [1, 2, 3, 4]:
-        radio.send_number(5)
+    global ack
+    ack = receivedNumber
+    # for testing
+    #if receivedNumber in [1, 2, 3, 4]:
+    #    radio.send_number(5)
 radio.on_received_number(on_received_number)
 
+# 0: idle
+# 1: send data
+# 2: wait 2s for ack value. If no ack, resend data
+# resend over 2 times -> turn led(0, 0) on in 1s
+# received ack -> turn led(1, 1) on in 1s
+sender_st = 0
+timer_sender = 200
+send_time = 0
+def send_data(data: int):
+    radio.send_number(data)
 
+led00_st = 0
+led00_timer = 100
+led11_st = 0
+led11_timer = 100
+
+def fsm_sender():
+    global sender_st, received, send_time, timer_sender, ack
+    global led00_st, led11_st
+    if sender_st == 0:
+        if received:
+            sender_st = 1
+            send_time = 0
+    elif sender_st == 1:
+        send_time += 1
+        send_data(received)
+        if ack == 5:
+            received = 0
+            sender_st = 0
+            ack = 0
+            led11_st = 1
+        else:
+            timer_sender = 200
+            sender_st = 2
+    elif sender_st == 2:
+        timer_sender -= 1
+        if ack == 5:
+            received = 0
+            sender_st = 0
+            ack = 0
+            led11_st = 1
+        elif timer_sender <= 0:
+            sender_st = 1
+        elif send_time >= 3:
+            received = 0
+            sender_st = 0
+            led00_st = 1
+    print(sender_st)
+def fsm_led(led_st, led_timer, x, y):
+    if led_st == 1:
+        led.plot(x, y)
+        led_timer -= 1
+        if led_timer <= 0:
+            led_st = 0
+            led_timer = 100
+    elif led_st == 0:
+        led.unplot(x, y)
+    return led_st, led_timer
+       
 def on_forever():
-    global flag, buf
-    if flag == 1:
-        flag = 0
-        if buf == 3:
-            basic.show_arrow(ArrowNames.WEST)
-        elif buf == 2:
-            basic.show_arrow(ArrowNames.EAST)
-        elif buf == 4:
-            basic.show_arrow(ArrowNames.SOUTH)
-        elif buf == 1:
-            basic.show_arrow(ArrowNames.NORTH)
+    global led00_st, led00_timer, led11_st, led11_timer
+    fsm_sender()
+
+    led00_st, led00_timer = fsm_led(led00_st, led00_timer, 0, 0)
+    
+    led11_st, led11_timer = fsm_led(led11_st, led11_timer, 1, 1)
     basic.pause(10)
 basic.forever(on_forever)
-
-# for testing
-def on_button_pressed_a():
-    # up
-    radio.send_number(1)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():
-    # left
-    radio.send_number(2)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_button_pressed_ab():
-    # right
-    radio.send_number(3)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def on_logo_event_pressed():
-    # down
-    radio.send_number(4)
-input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
--- main.ts
+++ main.ts
@@ -1,48 +1,112 @@
-//  receiver
+//  sender
 radio.setGroup(5)
-let flag = 0
-let buf = 1
+let received = 0
+input.onGesture(Gesture.LogoUp, function on_gesture_logo_up() {
+    
+    received = 1
+})
+input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
+    
+    received = 2
+})
+input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+    
+    received = 3
+})
+input.onGesture(Gesture.LogoDown, function on_gesture_logo_down() {
+    
+    received = 4
+})
+let ack = 0
+//  for testing
+// if receivedNumber in [1, 2, 3, 4]:
+//     radio.send_number(5)
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     
-    flag = 1
-    buf = receivedNumber
-    if ([1, 2, 3, 4].indexOf(buf) >= 0) {
-        radio.sendNumber(5)
-    }
-    
+    ack = receivedNumber
 })
-basic.forever(function on_forever() {
+//  0: idle
+//  1: send data
+//  2: wait 2s for ack value. If no ack, resend data
+//  resend over 2 times -> turn led(0, 0) on in 1s
+//  received ack -> turn led(1, 1) on in 1s
+let sender_st = 0
+let timer_sender = 200
+let send_time = 0
+function send_data(data: number) {
+    radio.sendNumber(data)
+}
+
+let led00_st = 0
+let led00_timer = 100
+let led11_st = 0
+let led11_timer = 100
+function fsm_sender() {
     
-    if (flag == 1) {
-        flag = 0
-        if (buf == 3) {
-            basic.showArrow(ArrowNames.West)
-        } else if (buf == 2) {
-            basic.showArrow(ArrowNames.East)
-        } else if (buf == 4) {
-            basic.showArrow(ArrowNames.South)
-        } else if (buf == 1) {
-            basic.showArrow(ArrowNames.North)
+    
+    if (sender_st == 0) {
+        if (received) {
+            sender_st = 1
+            send_time = 0
+        }
+        
+    } else if (sender_st == 1) {
+        send_time += 1
+        send_data(received)
+        if (ack == 5) {
+            received = 0
+            sender_st = 0
+            ack = 0
+            led11_st = 1
+        } else {
+            timer_sender = 200
+            sender_st = 2
+        }
+        
+    } else if (sender_st == 2) {
+        timer_sender -= 1
+        if (ack == 5) {
+            received = 0
+            sender_st = 0
+            ack = 0
+            led11_st = 1
+        } else if (timer_sender <= 0) {
+            sender_st = 1
+        } else if (send_time >= 3) {
+            received = 0
+            sender_st = 0
+            led00_st = 1
         }
         
     }
     
+    console.log(sender_st)
+}
+
+function fsm_led(led_st: number, led_timer: number, x: number, y: number): number[] {
+    if (led_st == 1) {
+        led.plot(x, y)
+        led_timer -= 1
+        if (led_timer <= 0) {
+            led_st = 0
+            led_timer = 100
+        }
+        
+    } else if (led_st == 0) {
+        led.unplot(x, y)
+    }
+    
+    return [led_st, led_timer]
+}
+
+basic.forever(function on_forever() {
+    
+    fsm_sender()
+    let ___tempvar4 = fsm_led(led00_st, led00_timer, 0, 0)
+    led00_st = ___tempvar4[0]
+    led00_timer = ___tempvar4[1]
+    let ___tempvar5 = fsm_led(led11_st, led11_timer, 1, 1)
+    led11_st = ___tempvar5[0]
+    led11_timer = ___tempvar5[1]
     basic.pause(10)
-})
-//  for testing
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    //  up
-    radio.sendNumber(1)
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    //  left
-    radio.sendNumber(2)
-})
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    //  right
-    radio.sendNumber(3)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
-    //  down
-    radio.sendNumber(4)
 })
